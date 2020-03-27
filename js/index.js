b0ee544b3fd44fe7d96319580f3733a66d1a5f69
spectrumsun <spectrumsun@hotmail.com>
2020-03-27 13:39:33 +0100

    add onclick to nav

--- js/index.js
+++ js/index.js
@@ -1,12 +1,16 @@
-window.onscroll = function() {
-  fixNavbar();
-};
-
 const navbar = document.getElementById("navbar");
 const sticky = navbar.offsetTop;
-
 const fixNavbar = () => {
   window.pageYOffset >= sticky
     ? navbar.classList.add("sticky")
-    : navbar.classList.remove("sticky")
+    : navbar.classList.remove("sticky");
 };
+
+const li = document.querySelectorAll(".nav-bar > ul > li");
+const addClassNav = e => {
+  document.querySelector(".active").classList.remove("active");
+  e.target.classList.add("active");
+};
+
+li.forEach(list => list.addEventListener("click", addClassNav));
+window.onscroll = () => fixNavbar();
